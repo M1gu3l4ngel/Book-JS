732217a015ebfa32476df27a2faa96f6867b3632
M1gu3l4ngel <miguelserrao15@gmail.com>
2022-11-22 00:20:41 -0600

    Subiendo proyecto + Agregando informacion

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Codigo de el Toggle
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+//searchBtn.addEventListener("click", () => {
+    //sidebar.classList.remove("close");
+//})
+
+// Codigo de AOS
+AOS.init({
+    easing: 'ease-out-back', duration: 2000
+})
